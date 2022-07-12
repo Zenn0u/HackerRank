74bafb456037b357865db8c752fb5e0bafcc746a
Zenn0u <talp97@hotmail.com>
2022-07-11 19:53:41 -0700

    Solved some questions

---
+++ Migratory Birds/python.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+#
+# Complete the 'migratoryBirds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def migratoryBirds(arr):
+    # Write your code here
+    c = Counter(arr)
+    ma = max(c.values())
+    mi = -1
+    print(c)
+    for i in c.keys():
+        if c[i] == ma:
+            if mi == -1:
+                mi = i
+            elif mi > i:
+                mi = i
+    return mi
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr_count = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = migratoryBirds(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
